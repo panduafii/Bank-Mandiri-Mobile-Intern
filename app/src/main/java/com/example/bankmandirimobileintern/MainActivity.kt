f6be8d912455df280b961f4327357f731dd17485
pandu <pandulombok@gmail.com>
2025-03-27 23:33:41 +0800

    Splash Screen

--- app/src/main/java/com/example/bankmandirimobileintern/MainActivity.kt
+++ app/src/main/java/com/example/bankmandirimobileintern/MainActivity.kt
@@ -11,37 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.bankmandirimobileintern.ui.theme.BankMandiriMobileInternTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             BankMandiriMobileInternTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BankMandiriMobileInternTheme {
-        Greeting("Android")
     }
 }
