623aa5e92aa533509204fda1f2b225188c176c1f
pandu <pandulombok@gmail.com>
2025-03-28 16:26:22 +0800

    2. On Boarding screen

--- app/src/main/java/com/example/bankmandirimobileintern/MainActivity.kt
+++ app/src/main/java/com/example/bankmandirimobileintern/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.bankmandirimobileintern.presentation.onboarding.OnBoardingScreen
 import com.example.bankmandirimobileintern.ui.theme.BankMandiriMobileInternTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +23,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
-            BankMandiriMobileInternTheme {
+            BankMandiriMobileInternTheme (dynamicColor = false) {
 
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
--- app/src/main/java/com/example/bankmandirimobileintern/ui/theme/Color.kt
+++ app/src/main/java/com/example/bankmandirimobileintern/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.example.bankmandirimobileintern.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Black = Color(0xFF1C1E21) //Dark Background
@@ -9,7 +11,6 @@ val DarkRed = Color(0xFFC30052) //Dark Error
 val LightRed = Color(0xFFFF84B7)
 
 val LightBlack = Color(0xFF3A3B3C) //Dark Surface
-
 
 val BlueGray = Color(0xFFA0A3BD)
 val WhiteGray = Color(0xFFB0B3B8)
